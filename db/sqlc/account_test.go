15ddcca3da89b5c4967c21a1097b0dde0420e3c2
shouta0715 <Myheroes0715@gmail.com>
2023-10-19 20:08:11 +0900

    pass test create accounts

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,5 +1,27 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
 
-func TestCreateAccount(t *testing.T) {}
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	args := CreateAccountParams{
+		Owner:    "Tom",
+		Balance:  100,
+		Currency: "USD",
+	}
+	account, err := testQueries.CreateAccount(context.Background(), args)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, args.Owner, account.Owner)
+	require.Equal(t, args.Balance, account.Balance)
+	require.Equal(t, args.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+}
