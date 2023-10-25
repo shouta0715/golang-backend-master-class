a7e00a78b1691f81fdf5113647fed7a0d94464ea
shouta0715 <Myheroes0715@gmail.com>
2023-10-25 17:23:37 +0900

    add middleware (#22)

--- api/server.go
+++ api/server.go
@@ -39,20 +39,22 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 func (server *Server) setupRouter() {
 	router := gin.Default()
 
-	// accounts api
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
-	router.DELETE("/accounts/:id", server.deleteAccount)
-
-	// transfer api
-	router.POST("/transfers", server.createTransfer)
-
 	// users api
 	router.POST("/users", server.createUser)
 
 	// auth api
 	router.POST("/auth/login", server.login)
+
+	authRoutes := router.Group("/").Use(authMiddleWare(server.maker))
+
+	// accounts api
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccount)
+	authRoutes.DELETE("/accounts/:id", server.deleteAccount)
+
+	// transfer api
+	authRoutes.POST("/transfers", server.createTransfer)
 
 	server.router = router
 
