6c304c4c618ae176d4a78e7930a76c281ce45239
shouta0715 <Myheroes0715@gmail.com>
2023-10-29 16:00:07 +0900

    add verify email (#59) (#60) (#61) (#62)

---
+++ db/sqlc/tx_verify_email.go
@@ -0,0 +1,49 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+)
+
+type VerifyEmailTxParams struct {
+	EmailId    int64
+	SecretCode string
+}
+
+type VerifyEmailTxResult struct {
+	User        User
+	VerifyEmail VerifyEmail
+}
+
+// VerifyEmailTx は口座から口座への送金を行う
+
+func (store *SQLStore) VerifyEmailTx(ctx context.Context, arg VerifyEmailTxParams) (
+	VerifyEmailTxResult, error) {
+	var result VerifyEmailTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.VerifyEmail, err = q.UpdateVerifyEmail(ctx, UpdateVerifyEmailParams{
+			ID:         arg.EmailId,
+			SecretCode: arg.SecretCode,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		result.User, err = q.UpdateUser(ctx, UpdateUserParams{
+			Username: result.VerifyEmail.Username,
+			IsEmailVerified: sql.NullBool{
+				Bool:  true,
+				Valid: true,
+			},
+		})
+
+		return err
+
+	})
+
+	return result, err
+}
